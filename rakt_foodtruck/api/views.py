15c61d7b568e262616b6b7976b332e04f4708504
JuniorGunner <csf.junior90@gmail.com>
2023-12-08 04:51:52 -0300

    map: rendering map, marking all foodtrucks

--- rakt_foodtruck/api/views.py
+++ rakt_foodtruck/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from .models import FoodTruck
 from .serializers import FoodTruckSerializer
+from django.shortcuts import render
 
 class FoodTruckViewSet(viewsets.ModelViewSet):
     queryset = FoodTruck.objects.all()
     serializer_class = FoodTruckSerializer
+
+
+def show_map(request):
+    return render(request, 'map.html')
--- rakt_foodtruck/src/urls.py
+++ rakt_foodtruck/src/urls.py
@@ -17,12 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import FoodTruckViewSet
+from api.views import FoodTruckViewSet, show_map
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'foodtrucks', FoodTruckViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-]
+    path('api/', include(router.urls)),
+    path('', show_map, name='show_map'),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
