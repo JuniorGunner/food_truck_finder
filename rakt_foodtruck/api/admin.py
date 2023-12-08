bd6e2496da8a8816cebc8f86afb042ca83dde5a0
JuniorGunner <csf.junior90@gmail.com>
2023-12-08 03:20:26 -0300

    config: register model o admin

--- rakt_foodtruck/api/admin.py
+++ rakt_foodtruck/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FoodTruck
 
-# Register your models here.
+class FoodTruckAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in FoodTruck._meta.get_fields()]
+
+admin.site.register(FoodTruck, FoodTruckAdmin)
