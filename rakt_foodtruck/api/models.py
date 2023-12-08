fb76191370c56f58f6b848daa14121187d9fac91
JuniorGunner <csf.junior90@gmail.com>
2023-12-08 02:03:46 -0300

    models: added FoodTruck model

--- rakt_foodtruck/api/models.py
+++ rakt_foodtruck/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodTruck(models.Model):
+    locationid = models.CharField(max_length=100, unique=True)
+    applicant = models.CharField(max_length=255)
+    facility_type = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    food_items = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    status = models.CharField(max_length=100)
+    schedule_url = models.URLField(max_length=255, blank=True)
+
+
+    def __str__(self):
+        return self.applicant
